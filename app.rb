9c9a309a78c75dd9505e39b04ad0d033877aa63a
Yann Klein <yann.klein@me.com>
2024-04-23 18:00:58 +0900

    fix style

--- app.rb
+++ app.rb
@@ -44,10 +44,10 @@ get "/:user_name" do
   json contrib
 end
 
-options "*" do
-  response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
-  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
-  response.headers["Access-Control-Allow-Origin"] = "*"
+options '*' do
+  response.headers['Allow'] = 'GET, PUT, POST, DELETE, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token'
+  response.headers['Access-Control-Allow-Origin'] = '*'
   200
 end
 
